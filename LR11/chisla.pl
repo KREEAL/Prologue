c5cbdb43aed3d8382cabb9a35b7f7fae43fa9415
Цветков Киллриал <k-real1337@mail.ru>
2022-03-30 21:16:38 +0300

    6

--- LR11/chisla.pl
+++ LR11/chisla.pl
@@ -1,30 +1,14 @@
-maxU(0,0):-!.
-maxU(X,M):-
-    X1 is X div 10,
-    maxU(X1,M1),
-    M2 is X mod 10,
-    (M2 > M1, M2 mod 3 =\= 0 -> M is M2; M is M1).
-	
 %15
-minU(0,10):-!.
+minU(0,9):-!.
 minU(X,M):-
-	/*X1 is X div 10,*/
-	minU(X div 10,M1),
+	X1 is X div 10,
+	minU(X1,M1),
 	M2 is X mod 10,
 	(M2<M1, M is M2;M is M1). 
 
-	
-minD(0,10):-!.
-minD(X,M):-
-	X1 is X div 10,
-	Cur is X mod 10,
-	M1 is 0,
-	if Cur < M then 
-		M1 = Cur 
-	else 
-		M1 = M 
-	end if,
-	minD(X1,M1).
-	
+%16
+minD(X,M):- minD(X,9,M).
+minD(0,M,M):-!.
+minD(X,Y,M):-D is X mod 10, X1 is X div 10,D < Y,!,minD(X1,D,M); X2 is X div 10, minD(X2,Y,M).
 %fhfhfh
 	
