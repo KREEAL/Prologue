3c9bab4d295bd82b16dc9a4069169d4cfb908b98
Цветков Киллриал <k-real1337@mail.ru>
2022-04-05 11:31:17 +0300

    Solved 1_2

--- LR14/LR14.pl
+++ LR14/LR14.pl
@@ -3,10 +3,10 @@ r_str(-1,A,A,N,N,1):-!.
 r_str(10,A,A,N,N,0):-!.
 r_str(X,A,B,N,K,Flag):-K1 is K+1,append(B,[X],B1),get0(X1),r_str(X1,A,B1,N,K1,Flag).
 
-read_list_str(List):-read_str(A,N,Flag),read_list_str([A],List,Flag).
+read_list_str(List):-read_str(A,_,Flag),read_list_str([A],List,Flag).
 read_list_str(List,List,1):-!.
 read_list_str(Cur_list,List,0):-
-	read_str(A,N,Flag),append(Cur_list,[A],C_l),read_list_str(C_l,List,Flag).
+	read_str(A,_,Flag),append(Cur_list,[A],C_l),read_list_str(C_l,List,Flag).
 	
 read_str(A,N):-get0(X),r_str(X,A,[],N,0).
 r_str(10,A,A,N,N):-!.
@@ -18,9 +18,30 @@ write_str([H|Tail]):-put(H),write_str(Tail).
 write_list_str([]):-!.
 write_list_str([H|T]):-write_str(H),nl,write_list_str(T).
 
+append([],X,X).
+append([X|T],Y,[X|T1]) :- append(T,Y,T1).
+
+%Задание 1
 %1
 symbolsstring(A,S):-symbolsstring(A,S,0).
 symbolsstring([],G,G):-!.
 symbolsstring([_|T],S,C):-C1 is C + 1, symbolsstring(T,S,C1),!.
 
-task1_5:-read_str(A,X,0),write_str(A),write(", "),write_str(A),write(", "),write_str(A),write(" , "),write(X).
+task1_1:-read_str(A,X,0),write_str(A),write(", "),write_str(A),write(", "),write_str(A),write(" , "),write(X).
+
+%2
+task1_2:-read_str(A,_),countwords(A,K),write(K).
+
+countwords(A,C):-countwords(A,0,C).
+countwords([],K,K):-!.
+countwords(Str,I,K):-skipspace(Str,Snew),getword(Snew,Word,Snew1),Word \=[],I1 is I+1,countwords(Snew1,I1,K),!.
+countwords(_,K,K).
+
+skipspace([32|T],A1):-skipspace(T,A1),!.
+skipspace(A1,A1).
+
+getword([],[],[]):-!.
+getword(A,Word,A2):-getword(A,[],Word,A2).
+getword([],Word,Word,[]).
+getword([32|T],Word,Word,T):-!.
+getword([H|T],W,Word,A2):-append(W,[H],W1),getword(T,W1,Word,A2).
